6bf636435076480ea10277fd0d81c92571704454
FlashCodX <luis.pinto.trading@gmail.com>
2022-04-22 22:34:49 +0100

    Readme Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App";
-const root = createRoot(document.getElementById("root") as HTMLElement); // createRoot(container!) if you use TypeScript
+const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(<App />);
